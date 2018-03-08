3316dd4d07dc7104d1b520b06585fc4a6787964b
Emmanuel Sindani <sindaniemmanuel1919@gmail.com>
2018-03-08 04:42:56 -0800

    changes

--- admin/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost", "id4438251_chirowa", "soen30010010", "id4438251_chirowa");
-	
-    //check if connection to the database was estblished
-	if(mysqli_connect_errno())
-	{
-	    echo "Failed to connect to the database: " . mysqli_connect_error();
-	}
-?>
