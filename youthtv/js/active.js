c5711aef08c8d3f2dd970bf371e7ca0dad4293ad
crimsoftit <crimsoftit@gmail.com>
2019-08-19 02:31:02 +0300

    UI templates redesign

--- youthtv/js/active.js
+++ youthtv/js/active.js
@@ -12,34 +12,40 @@
 
     // :: 2.0 Nav Active Code
     if ($.fn.classyNav) {
-        $('#magNav').classyNav();
+        $('#newsboxNav').classyNav();
     }
 
-    // :: 3.0 Sticky Active Code
-    if ($.fn.sticky) {
-        $("#sticker").sticky({
-            topSpacing: 0
+    // :: 3.0 Newsticker Active Code
+    if ($.fn.simpleTicker) {
+        $.simpleTicker($("#breakingNewsTicker"), {
+            speed: 1000,
+            delay: 3000,
+            easing: 'swing',
+            effectType: 'roll'
         });
     }
 
     // :: 4.0 Sliders Active Code
     if ($.fn.owlCarousel) {
 
-        var welcomeSlides = $('.hero-area');
+        var welcomeSlide = $('.hero-post-slides');
+        var videoSlides = $('.video-slides');
+        var albumSlides = $('.albums-slideshow');
 
-        welcomeSlides.owlCarousel({
+        welcomeSlide.owlCarousel({
             items: 1,
             margin: 0,
             loop: true,
-            nav: true,
-            navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
+            nav: false,
             dots: false,
             autoplay: true,
-            autoplayTimeout: 5000,
-            smartSpeed: 1000
+            autoplayTimeout: 7000,
+            smartSpeed: 1000,
+            animateIn: 'fadeIn',
+            animateOut: 'fadeOut'
         });
 
-        welcomeSlides.on('translate.owl.carousel', function () {
+        welcomeSlide.on('translate.owl.carousel', function () {
             var slideLayer = $("[data-animation]");
             slideLayer.each(function () {
                 var anim_name = $(this).data('animation');
@@ -47,8 +53,8 @@
             });
         });
 
-        welcomeSlides.on('translated.owl.carousel', function () {
-            var slideLayer = welcomeSlides.find('.owl-item.active').find("[data-animation]");
+        welcomeSlide.on('translated.owl.carousel', function () {
+            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
             slideLayer.each(function () {
                 var anim_name = $(this).data('animation');
                 $(this).addClass('animated ' + anim_name).css('opacity', '1');
@@ -65,110 +71,99 @@
             $(this).css('animation-duration', anim_dur);
         });
 
-        $('.trending-post-slides').owlCarousel({
+        videoSlides.owlCarousel({
             items: 3,
             margin: 30,
             loop: true,
-            nav: true,
-            navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
             dots: false,
             autoplay: true,
-            autoplayTimeout: 4000,
-            smartSpeed: 1000,
+            nav: true,
+            navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
             responsive: {
                 0: {
                     items: 1
                 },
-                992: {
+                576: {
                     items: 2
                 },
-                1500: {
+                992: {
                     items: 3
-                }
-            }
-        });
-
-        $('.featured-video-posts-slide').owlCarousel({
-            items: 1,
-            margin: 0,
-            loop: true,
-            nav: true,
-            navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
-            dots: false,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            smartSpeed: 1000
-        });
-
-        $('.most-viewed-videos-slide').owlCarousel({
-            items: 3,
-            margin: 30,
-            loop: true,
-            nav: true,
-            navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
-            dots: false,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                992: {
-                    items: 2
-                },
-                1500: {
-                    items: 3
-                }
-            }
-        });
-
-        $('.sports-videos-slides').owlCarousel({
-            items: 2,
-            margin: 30,
-            loop: true,
-            nav: true,
-            navText: ['<i class="ti-angle-left"></i>', '<i class="ti-angle-right"></i>'],
-            dots: false,
-            autoplay: true,
-            autoplayTimeout: 4000,
-            smartSpeed: 1000,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                992: {
-                    items: 2
-                },
-                1200: {
-                    items: 1
-                },
-                1500: {
-                    items: 2
                 }
             }
         });
     }
+    
+    // :: 5.0 Video Active Code
+    if ($.fn.magnificPopup) {
+        $('.video-btn').magnificPopup({
+            disableOn: 0,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: true,
+            fixedContentPos: false
+        });
+    }
 
-    // :: 5.0 ScrollUp Active Code
+    // :: 6.0 ScrollUp Active Code
     if ($.fn.scrollUp) {
         browserWindow.scrollUp({
             scrollSpeed: 1500,
-            scrollText: '<i class="ti-angle-up"></i>'
+            scrollText: '<i class="fa fa-angle-up"></i>'
         });
     }
 
-    // :: 6.0 Tooltip Active Code
-    if ($.fn.tooltip) {
-        $('[data-toggle="tooltip"]').tooltip();
+    // :: 7.0 CounterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
     }
 
-    // :: 7.0 Prevent Default a Click
-    $('a[href="#"]').on('click', function (e) {
-        e.preventDefault();
+    // :: 8.0 Progress Bar Active Code
+    if ($.fn.circleProgress) {
+        $('#circle').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#a2a2a2',
+            thickness: '3',
+            reverse: true
+        });
+        $('#circle2').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#a2a2a2',
+            thickness: '3',
+            reverse: true
+        });
+        $('#circle3').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#a2a2a2',
+            thickness: '3',
+            reverse: true
+        });
+        $('#circle4').circleProgress({
+            size: 160,
+            emptyFill: "rgba(0, 0, 0, .0)",
+            fill: '#a2a2a2',
+            thickness: '3',
+            reverse: true
+        });
+    }
+
+    // :: 9.0 Tooltip Active Code
+    if ($.fn.tooltip) {
+        $('[data-toggle="tooltip"]').tooltip()
+    }
+
+    // :: 10.0 Prevent Default a Click
+    $('a[href="#"]').on('click', function ($) {
+        $.preventDefault();
     });
 
-    // :: 8.0 Wow Active Code
+    // :: 11.0 Wow Active Code
     if (browserWindow.width() > 767) {
         new WOW().init();
     }
